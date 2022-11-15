3092027cb148e75e6a21251ee3b02c327ef82584
Nilton S Ferreira <nsfgeo@hotmail.com>
2022-11-15 00:28:41 -0300

    Criação de API de filmes

---
+++ FilmesAPI/Data/Dtos/ReadUpdateFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo de 'Titulo' é obrigatório.")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo de 'Diretor' é obrigatório.")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O campo de 'Gênero' não pode ser maior que 30 caracteres.")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração do filme deve ser entre 1 e 600 minutos.")]
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
